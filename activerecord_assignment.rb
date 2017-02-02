a328981206e8cc5c3fdac7c304e3ac1e17991f66
sylvia villegas <sylvia@svillegas.com>
2017-02-02 08:16:26 -0500

    Finish exercise

--- activerecord_assignment.rb
+++ activerecord_assignment.rb
@@ -5,19 +5,22 @@
 
 
 # 1a) Find the genre with the name "Hip Hop/Rap".
-
-
+Genre.find_by(name: "Hip Hop/Rap")
+# => #<Genre id: 17, name: "Hip Hop/Rap", created_at: "2013-01-20 02:04:31", updated_at: "2014-01-29 22:14:08">
 
 # 1b) Count how many tracks belong to the "Hip Hop/Rap" genre
-
-
+Track.where(genre_id: 17).count
+# => 35
 
 # 2) Find the total amount of time required to listen to all the tracks in the database.
-
-
+Track.sum(:milliseconds)
+# => 1378575827
 
 # 3a) Find the highest price of any track that has the media type "MPEG audio file".
-
+MediaType.where(name: "MPEG audio file")
+# => #<MediaType id: 1, name: "MPEG audio file", created_at: "2006-11-24 08:21:33", updated_at: "2014-01-29 22:14:22">
+Track.where(media_type_id: 1).maximum(:unit_price)
+# => #<BigDecimal:1564428,'0.99E0',9(18)>
 
 
 # 3b) Find the name of the most expensive track that has the media type "MPEG audio file".
@@ -25,10 +28,11 @@
 
 
 # 4) Find the 2 oldest artists.
-
-
+Artist.order(:created_at).limit(2)
+# => [#<Artist id: 258, name: "Les Arts Florissants & William Christie", created_at: "2004-02-13 06:08:36", updated_at: "2014-01-29 22:10:22">, #<Artist id: 31, name: "Baby Consuelo", created_at: "2004-02-16 22:04:39", updated_at: "2014-01-29 22:10:22">]
 
 ### Stretch Exercise (Active Record Query Interface)
 
 
 # 1) Find all the albums whose titles start with B.
+Album.find_by_sql("SELECT * FROM albums WHERE title LIKE 'B%'")
--- sql_assignment.sql
+++ sql_assignment.sql
@@ -5,19 +5,92 @@
 -- below each of the following questions:
 
 -- 1) Find the albums recorded by the artist Queen.
-
+SELECT * FROM artists WHERE name = 'Queen';
+-- id = 3
+SELECT title FROM albums WHERE artist_id = 51;
+-- Greatest Hits II
+-- Greatest Hits I
+-- News Of The World
 
 
 -- 2) [Count](http://www.w3schools.com/sql/sql_func_count.asp) how many tracks belong to the media type "Protected MPEG-4 video file".
-
+SELECT id FROM media_types WHERE name = 'Protected MPEG-4 video file';
+-- id = 3
+SELECT COUNT(*) FROM tracks WHERE media_type_id = 3;
+-- count = 214
 
 
 -- 3) Find the least expensive track that has the genre "Electronica/Dance".
-
+SELECT id FROM genres WHERE name = 'Electronica/Dance';
+-- id = 15
+SELECT name, unit_price FROM tracks WHERE genre_id = 15;
+--            name             | unit_price
+-------------------------------+------------
+-- Instinto Colectivo          |       0.99
+-- Chapa o Coco                |       0.99
+-- Prostituta                  |       0.99
+-- Eu So Queria Sumir          |       0.99
+-- Tres Reis                   |       0.99
+-- Um Lugar ao Sol             |       0.99
+-- Batalha Naval               |       0.99
+-- O Misterio do Samba         |       0.99
+-- Armadura                    |       0.99
+-- Na Ladeira                  |       0.99
+-- Carimbo                     |       0.99
+-- Catimbo                     |       0.99
+-- Chega no Suingue            |       0.99
+-- Mun-Ra                      |       0.99
+-- Freestyle Love              |       0.99
+-- Distance                    |       0.99
+-- One Step Beyond             |       0.99
+-- Stillness In Time           |       0.99
+-- Journey To Arnhemland       |       0.99
+-- Just Another Story          |       0.99
+-- Todo o Carnaval tem seu Fim |       0.99
+-- Funk de Bamba               |       0.99
+-- Half The Man                |       0.99
+-- Light Years                 |       0.99
+-- Manifest Destiny            |       0.99
+-- The Kids                    |       0.99
+-- Mr. Moon                    |       0.99
+-- Scam                        |       0.99
+-- Morning Glory               |       0.99
+-- Space Cowboy                |       0.99
 
 
 -- 4) Find the all the artists whose names start with A.
-
+SELECT name FROM artists WHERE name LIKE 'A%'
+-- Aerosmith
+-- Alice In Chains
+-- Antônio Carlos Jobim
+-- Apocalyptica
+-- Azymuth
+-- Aquaman
+-- AC/DC
+-- Alanis Morissette
+-- Audioslave
+-- Avril Lavigne
+-- Aisha Duo
+-- Aaron Goldberg
+-- Academy of St. Martin in the Fields Chamber Ensemble & Sir Neville Marriner
+-- Academy of St. Martin in the Fields, John Birch, Sir Neville Marriner & Sylvia McNair
+-- Aaron Copland & London Symphony Orchestra
+-- Antal Doráti & London Symphony Orchestra
+-- Amy Winehouse
+-- Adrian Leaper & Doreen de Feis
+-- Accept
+-- Aerosmith & Sierra Leone's Refugee Allstars
+-- Alberto Turco & Nova Schola Gregoriana
+-- Anne-Sophie Mutter, Herbert Von Karajan & Wiener Philharmoniker
+-- Academy of St. Martin in the Fields & Sir Neville Marriner
+-- Academy of St. Martin in the Fields, Sir Neville Marriner & William Bennett
+-- Academy of St. Martin in the Fields, Sir Neville Marriner & Thurston Dart
+-- A Cor Do Som
 
 
 -- 5) Find all the tracks that belong to playlist 1.
+SELECT tracks.name
+FROM playlists_tracks
+JOIN tracks
+ON tracks.id = playlists_tracks.track_id
+WHERE playlists_tracks.playlist_id = 1;
